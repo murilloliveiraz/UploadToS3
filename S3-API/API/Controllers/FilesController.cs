756a3756cf3c119c8b4dc0018b922e8289f744dd
Murillo Oliveira <murillo1047@gmail.com>
2024-08-09 14:41:53 -0300

    feat: method that upload a file to controller

---
+++ S3-API/API/Controllers/FilesController.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly IAmazonS3 _s3Client;
+
+        public FilesController(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFileAsync(IFormFile file, string bucketName, string? prefix)
+        {
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new PutObjectRequest()
+            {
+                BucketName = bucketName,
+                Key = string.IsNullOrEmpty(prefix) ? file.FileName : $"{prefix?.TrimEnd('/')}/{file.FileName}",
+                InputStream = file.OpenReadStream()
+            };
+            request.Metadata.Add("Content-Type", file.ContentType);
+            await _s3Client.PutObjectAsync(request);
+            return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
+        }
+
+    }
+}
