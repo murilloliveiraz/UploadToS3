064579f99fd5bc54e45a08d1511f5e57d4270d89
Murillo Oliveira <murillo1047@gmail.com>
2024-08-09 15:12:25 -0300

    feat: method that get, get by, and delete files from s3

--- S3-API/API/Controllers/FilesController.cs
+++ S3-API/API/Controllers/FilesController.cs
@@ -1,6 +1,6 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
-using Microsoft.AspNetCore.Http;
+using API.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -33,5 +33,50 @@ namespace API.Controllers
             return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllFilesAsync(string bucketName, string? prefix)
+        {
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new ListObjectsV2Request()
+            {
+                BucketName = bucketName,
+                Prefix = prefix
+            };
+            var result = await _s3Client.ListObjectsV2Async(request);
+            var s3Objects = result.S3Objects.Select(s =>
+            {
+                var urlRequest = new GetPreSignedUrlRequest()
+                {
+                    BucketName = bucketName,
+                    Key = s.Key,
+                    Expires = DateTime.UtcNow.AddMinutes(1)
+                };
+                return new S3ObjectDTO()
+                {
+                    Name = s.Key.ToString(),
+                    PresignedUrl = _s3Client.GetPreSignedURL(urlRequest),
+                };
+            });
+            return Ok(s3Objects);
+        }
+
+        [HttpGet("preview")]
+        public async Task<IActionResult> GetFileByKeyAsync(string bucketName, string key)
+        {
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var s3Object = await _s3Client.GetObjectAsync(bucketName, key);
+            return File(s3Object.ResponseStream, s3Object.Headers.ContentType);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFileAsync(string bucketName, string key)
+        {
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist");
+            await _s3Client.DeleteObjectAsync(bucketName, key);
+            return NoContent();
+        }
     }
 }
---
+++ S3-API/API/DTO/S3ObjectDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTO
+{
+    public class S3ObjectDTO
+    {
+        public string? Name { get; set; }
+        public string? PresignedUrl { get; set; }
+    }
+}
